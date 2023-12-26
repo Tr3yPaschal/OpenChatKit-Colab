618f35c68e786bd13a13732281d0f9058760f15c
Tr3yPaschal <trye#nerdskoolmedia.com>
2023-12-26 12:05:02 -0700

    Wait for >>>

--- inference/start.py
+++ inference/start.py
@@ -9,19 +9,26 @@ process = subprocess.Popen(
     stdin=subprocess.PIPE, 
     stdout=subprocess.PIPE, 
     stderr=subprocess.PIPE, 
-    text=True)
+    text=True, 
+    bufsize=1,  # Line buffered
+    universal_newlines=True)
 
-# Give it some time to start up (2 minutes)
-time.sleep(120)
+prompt_encountered = False
+while not prompt_encountered:
+    # Read lines from the process output
+    output_line = process.stdout.readline()
+    print(output_line, end='')  # Print the output line
+    if ">>>" in output_line:
+        prompt_encountered = True
 
 # Send the api_inference command to bot.py
 command = 'api_inference {"prompt": "Hello, how are you?", "max_new_tokens": 50, "do_sample": true, "temperature": 0.7, "top_k": 40}\n'
 process.stdin.write(command)
 process.stdin.flush()
 
-# Read the output (may need adjustments based on how bot.py outputs data)
-output = process.stdout.readline()
-print(output)
+# Read the response after the command
+response = process.stdout.readline()
+print(response)
 
 # Close the process
 process.terminate()
