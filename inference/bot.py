6d7ad49dde0c746c106c743020e78c2f1b734b72
Tr3yPaschal <trye#nerdskoolmedia.com>
2023-12-27 10:41:46 -0700

    Hopeful

--- inference/bot.py
+++ inference/bot.py
@@ -8,6 +8,7 @@ INFERENCE_DIR = os.path.dirname(os.path.abspath(__file__))
 # TODO: PYTHONPATH hacks are never a good idea. clean this up later
 sys.path.append(os.path.join(INFERENCE_DIR, '..'))
 
+import cmd
 import torch
 import argparse
 import conversation as convo
@@ -39,6 +40,7 @@ class StopWordsCriteria(StoppingCriteria):
         if self._stream_callback:
             if first:
                 text = text.lstrip()
+            # buffer tokens if the partial result ends with a prefix of a stop word, e.g. "<hu"
             for stop_word in self._stop_words:
                 for i in range(1, len(stop_word)):
                     if self._partial_result.endswith(stop_word[0:i]):
@@ -102,7 +104,34 @@ class ChatModel:
 
         return output
 
-# ... (Rest of the code for OpenChatKitShell class and main() function)
+class StopWordsCriteria(StoppingCriteria):
+    def __init__(self, tokenizer, stop_words, stream_callback):
+        self._tokenizer = tokenizer
+        self._stop_words = stop_words
+        self._partial_result = ''
+        self._stream_buffer = ''
+        self._stream_callback = stream_callback
+
+    def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor, **kwargs) -> bool:
+        first = not self._partial_result
+        text = self._tokenizer.decode(input_ids[0, -1])
+        self._partial_result += text
+        for stop_word in self._stop_words:
+            if stop_word in self._partial_result:
+                return True
+        if self._stream_callback:
+            if first:
+                text = text.lstrip()
+            # buffer tokens if the partial result ends with a prefix of a stop word, e.g. "<hu"
+            for stop_word in self._stop_words:
+                for i in range(1, len(stop_word)):
+                    if self._partial_result.endswith(stop_word[0:i]):
+                        self._stream_buffer += text
+                        return False
+            self._stream_callback(self._stream_buffer + text)
+            self._stream_buffer = ''
+        return False
+    
 
 if __name__ == '__main__':
     main()
