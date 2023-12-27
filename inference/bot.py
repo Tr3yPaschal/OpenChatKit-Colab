ebfff007bf0fe724d60236269371ef20d6f52424
Tr3yPaschal <trye#nerdskoolmedia.com>
2023-12-27 15:13:00 -0700

    Unraveling a mess

--- inference/bot.py
+++ inference/bot.py
@@ -91,25 +91,26 @@ class ChatModel:
 
     def do_inference(self, prompt, max_new_tokens, do_sample, temperature, top_k, stream_callback=None):
         stop_criteria = StopWordsCriteria(self._tokenizer, [self.human_id], stream_callback)
-        inputs = (
-            self._tokenizer(text=prompt, return_tensors='pt')
-            .to(self._model.device)
-        )
+        inputs = self._tokenizer(
+            text=[prompt],  # Make sure to pass the prompt as a list of strings
+            return_tensors='pt'
+        ).to(self._model.device)
         outputs = self._model.generate(
-            **inputs,
-            max_new_tokens=max_new_tokens,
+            input_ids=inputs["input_ids"],
+            max_length=max_new_tokens,
             do_sample=do_sample,
             temperature=temperature,
             top_k=top_k,
             pad_token_id=self._tokenizer.eos_token_id,
             stopping_criteria=StoppingCriteriaList([stop_criteria]),
         )
-        output = self._tokenizer.batch_decode(outputs)[0]
+        output = self._tokenizer.batch_decode(outputs, skip_special_tokens=True)[0]
 
         # remove the context from the output
         output = output[len(prompt):]
 
         return output
+
 
 
 @app.route('/', methods=['POST'])
